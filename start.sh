7ce87751b657673f67ae911ca17334fca1410229
lede <jude_kun@qq.com>
2018-11-14 22:22:38 +0800

    dd

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
 
 while true; do
     pid=`ps -ef|grep vod_httpserver|grep -v grep|awk '{print $2}'`
     if [ -z "$pid" ]; then
-        ./bin/portal
+        ./xware/portal
     else
         sleep 60
     fi
